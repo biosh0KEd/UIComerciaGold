469e1780a580127e0cbb361368285a9fef130445
Efrain Giron <efrainbond@hotmail.com>
2022-04-07 14:31:34 -0500

    Ya se puede crear varias ventanas, falta eliminarlas

--- UIComercia/Services/PaginaActual.cs
+++ UIComercia/Services/PaginaActual.cs
@@ -1,18 +1,39 @@
-﻿namespace UIComercia.Services
+﻿using Microsoft.AspNetCore.Components;
+
+namespace UIComercia.Services
 {
 	public class PaginaActual
 	{
-		public string NombrePaginaActual { get; set; } = "Home";
-		public Type TypePaginaActual { get; set; } = Activator.CreateInstance("UIComercia", "UIComercia.Pages.Home").Unwrap().GetType();
+		private int contador = 2; 
+		public int Numero = 1;
+		public string NombrePaginaActual { get; set; } = "Home";		
+		public Type TypePaginaActual { get; set; } = Activator.CreateInstance("UIComercia", $"UIComercia.Pages.Home").Unwrap().GetType();		
 
-		public void SetNombrePaginaActual(string name)
+		public void CrearNuevaPagina(string name)
+		{			
+			NombrePaginaActual = name;
+			Numero = contador;
+			contador++;
+			TypePaginaActual = Activator.CreateInstance("UIComercia", $"UIComercia.Pages.{name}").Unwrap().GetType();
+			NotifyStateChanged();			
+		}
+
+		public Pagina GetPagina()
 		{
-			if (!string.Equals(NombrePaginaActual, name))
+			return new Pagina
 			{
-				NombrePaginaActual = name;
-				TypePaginaActual = Activator.CreateInstance("UIComercia", $"UIComercia.Pages.{name}").Unwrap().GetType();
-				NotifyStateChanged();
-			}
+				Nombre = NombrePaginaActual,
+				Tipo = TypePaginaActual,
+				Numero = Numero
+			};
+		}		
+
+		public void cambiarVentana(Pagina pagina)
+		{
+			this.NombrePaginaActual = pagina.Nombre;
+			this.Numero = pagina.Numero;
+			this.TypePaginaActual = pagina.Tipo;
+			NotifyStateChanged();
 		}
 
 		public event Action OnChange;
--- UIComercia/Services/PaginasAbiertas.cs
+++ UIComercia/Services/PaginasAbiertas.cs
@@ -2,5 +2,24 @@
 {
     public class PaginasAbiertas
     {
+        public List<Pagina> Paginas { get; set; }
+
+        public PaginasAbiertas()
+		{            
+            this.Paginas = new List<Pagina>();
+            Paginas.Add(new Pagina
+            {
+                Numero = 1,
+                Nombre = "Home",
+                Tipo = Activator.CreateInstance("UIComercia", $"UIComercia.Pages.Home").Unwrap().GetType()
+            });            
+		}
     }
+
+    public class Pagina
+	{        
+        public string Nombre { get; set; }
+        public Type Tipo { get; set; }
+        public int Numero { get; set; }
+	}
 }
